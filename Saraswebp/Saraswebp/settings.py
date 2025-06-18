0b98094c294fbb7d2e4b5f510232a474ce31eb23
vamsi krishna <iamvkrishna24@gmail.com>
2025-06-18 16:58:37 +0530

    whitelisted render for testing

--- Saraswebp/Saraswebp/settings.py
+++ Saraswebp/Saraswebp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0xpbwk6-hsddf+#(6mqzb(x5xnx0x^g3m!!oqaeiduwmpj-(ra
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saras-web.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
